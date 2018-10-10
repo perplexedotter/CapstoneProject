ed93d81c72adc7cf58ebbec423cce0dbe6822d54
Paul Grimes <grimesp@oregonstate.edu>
2018-10-10 09:23:44 -0700

    Basic Stats

---
+++ Assets/Scripts/Unit Module/FighterStat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterStat : MonoBehaviour {
+    public List<BaseStat> stats = new List<BaseStat>();
+    void Start()
+    {
+        stats.Add(new BaseStat(100, "Health", "Amount of damage a unit can recieve before destruction"));
+        stats[0].addStatBonus(new BonusStat(5));
+        stats[0].addStatBonus(new BonusStat(15));
+        Debug.Log(stats[0].getFinalValue());
+    }
+}
