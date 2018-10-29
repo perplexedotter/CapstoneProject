0b59b22482c95f64245298b831e78d4895432765
Paul Grimes <grimesp@oregonstate.edu>
2018-10-29 16:05:31 -0700

    Ship Builder Update

---
+++ Assets/Scripts/UI/rotateShip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateShip : MonoBehaviour {
+
+    float rotSpeed = 20;
+
+    private void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        this.gameObject.transform.RotateAround(Vector3.up, -rotX);
+        this.gameObject.transform.RotateAround(Vector3.right, rotY);
+        Debug.Log(this);
+        Debug.Log(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+}
