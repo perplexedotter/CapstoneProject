21721c0c158eeb1d26becada01cb8024a187ad88
Adrian Towery <adrian.towery@gmail.com>
2018-11-12 10:51:02 -0800

    add intro sounds and connect shipbuilder to menu

---
+++ Assets/Scripts/Sounds/DontDestroySound.cs
@@ -0,0 +1,26 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ using UnityEngine.SceneManagement;
+ 
+ public class DontDestroySound : MonoBehaviour {
+ 
+     void Awake ()
+     {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+ 
+        DontDestroyOnLoad(this.gameObject);
+ 
+     }
+ 
+     void Update()
+     {
+		Scene scene = SceneManager.GetActiveScene();
+        Debug.Log("Active scene is '" + scene.name + "'.");
+        if (scene.name == "map1")
+        {
+            Destroy(this.gameObject);
+        }
+     }
+ }
