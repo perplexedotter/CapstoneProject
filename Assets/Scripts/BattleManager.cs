639b840983eb6ba34760f26e462a802311d86668
perplexedotter <panda3125@gmail.com>
2018-11-23 18:10:18 -0500

    Final conflict fix... hopefully

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -170,7 +170,7 @@ public class BattleManager : MonoBehaviour {
         //keep info by activeunit TODO: put in own function
         //Vector3 statusPos = Camera.main.WorldToScreenPoint(activeUnit.transform.position);
         //statusText.transform.position = statusPos;
-        //statusText.text = "HP: " + activeUnit.DamageUnit(0) + "\nType: " + activeUnit.GetShipType() + "\nMods: " + statusBarMods;
+        //statusText.text = "HP: " + activeUnit.DamageUnit(0) +  "\nType: " + activeUnit.GetShipType() + "\nMods: " + statusBarMods;
         if(victoryType == VictoryType.waveSurvival)
         {
             roundsLeftText.text = "Survive The Waves!\n     Rounds Left: " + (RoundsToSurvive - roundNumber).ToString();
